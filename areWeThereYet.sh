3a9c8965a8b096d962ac42a38c21bd4ff2386436
Luis Alves <LAlves@wsgc.com>
2019-04-19 10:11:47 -0700

    Sending the status to be checked

--- areWeThereYet.sh
+++ areWeThereYet.sh
@@ -1,27 +1,90 @@
 #!/bin/sh
 
-if [ "$1" == "" ] ; then
-    echo "ERROR! Missing application id."
-    echo "Usage: ./areWeThereYet.sh <APPLICATION_ID>\n"
+callSite() {
+  {
+    echo "Calling website status with application id: $1..."
+    ANSWER=`curl -X POST -F 'appReceiptNum='$1 -F 'initCaseSearch=CHECK STATUS' https://egov.uscis.gov/casestatus/mycasestatus.do`
+    echo "Done."
+  } 2> /dev/null
+
+  echo "Parsing response (expected response: $2)..."
+  {
+    if echo "$ANSWER" | grep -E "($2</h1>)"; then
+      STATUS="Status: still the same ("$2")";
+    else
+      STATUS="STATUS CHANGED!!!!!!!!!!";
+    fi
+  } 2> /dev/null
+  echo "Done."
+
+  echo $STATUS
+
+}
+
+showHelp() {
+  usage="  usage: ./areWeThereYet.sh -a <APPLICATION_ID> -s <EXPECTED_STATUS>
+
+  List of status
+        1              Request For Evidence Was Received
+        2              Request for Additional Evidence Was Mailed
+        \n
+  example: ./areWeThereYet.sh -a WAC1234567890 -s 1
+  \n"
+
+  echo "$usage"
+}
+
+REQUEST_FOR_EVIDENCE_WAS_RECEIVED="Request For Evidence Was Received"
+REQUEST_FOR_ADDITIONAL_EVIDENCE_WAS_MAILED="Request for Additional Evidence Was Mailed"
+
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+# Initialize our own variables:
+application_id=""
+expected_status=""
+
+while getopts "h?s:a:" opt; do
+    case "$opt" in
+    h|\?)
+        showHelp
+        exit 0
+        ;;
+    s)  expected_status=$OPTARG
+        ;;
+    a)  application_id=$OPTARG
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "${1:-}" = "--" ] && shift
+
+if [ "$application_id" == "" ] ; then
+    echo "Missing -a <APPLICATION_ID>!"
+    showHelp
     exit 0
 fi
 
-{
-  echo "Calling website status with application id: $1..."
-  ANSWER=`curl -X POST -F 'appReceiptNum='$1 -F 'initCaseSearch=CHECK STATUS' https://egov.uscis.gov/casestatus/mycasestatus.do`
-  echo "Done."
-} 2> /dev/null
+if [ "$expected_status" == "" ] ; then
+    echo "Missing -s <EXPECTED_STATUS>!"
+    showHelp
+    exit 0
+fi
 
-echo "Parsing response..."
-{
-  if echo "$ANSWER" | grep -E "(Request For Evidence Was Received</h1>)"; then
-    STATUS="Status: still the same (Request For Evidence Was Received)";
-  elif echo "$ANSWER" | grep -E "(Request for Additional Evidence Was Mailed</h1>)"; then
-    STATUS="Status: still the same (Request for Additional Evidence Was Mailed)";
-  else
-    STATUS="STATUS CHANGED!!!!!!!!!!";
-  fi
-} 2> /dev/null
-echo "Done."
+case $expected_status in
+1)
+  expected_status=$REQUEST_FOR_EVIDENCE_WAS_RECEIVED
+  ;;
+2)
+  expected_status=$REQUEST_FOR_ADDITIONAL_EVIDENCE_WAS_MAILED
+  ;;
+*)
+  echo "ERROR! Unexpected status."
+  showHelp
+  exit 0
+esac
 
-echo $STATUS
+echo "status $expected_status appid $application_id"
+callSite "$application_id" "$expected_status"
